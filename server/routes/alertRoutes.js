174f1f02cc95bc5a073172980a75863ffff5e4b3
Santiago <volingtav7@gmail.com>
2026-07-13 21:48:50 -0300

    fix: middleware import in routes

--- server/routes/alertRoutes.js
+++ server/routes/alertRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const alertService = require('../services/alertService');
-const authMiddleware = require('../middlewares/authMiddleware');
+const { verifyToken } = require('../middlewares/authMiddleware');
 
-router.use(authMiddleware);
+router.use(verifyToken);
 
 // GET /api/alerts
 router.get('/', async (req, res) => {
--- server/routes/favoritesRoutes.js
+++ server/routes/favoritesRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../config/db');
-const authMiddleware = require('../middlewares/authMiddleware');
+const { verifyToken } = require('../middlewares/authMiddleware');
 
-router.use(authMiddleware);
+router.use(verifyToken);
 
 // GET /api/favorites
 router.get('/', async (req, res) => {
